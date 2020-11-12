2257f681ceaa754191c2a004eb5bd9fa46ae4d28
Nativi <juanativi95@gmail.com>
2020-11-12 16:31:38 -0600

    primer commit proyecto ionic con formulario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  formulario: FormGroup;
+
+  constructor(public formBuilder: FormBuilder) {
+    this.formulario = this.createForm();
+  }
+
+  private createForm() {
+    return this.formBuilder.group({
+      n1: ["", Validators.required],
+      n2: ["", Validators.required]
+    });
+  }
+
+  CalcularOperaciones() {
+    console.log("++++++++++++=boton++++++++++++")
+    console.log(this.formulario.value);
+  }
 
 }
